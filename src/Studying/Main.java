b889df49b6785d817a900331b778bfaf6b788b90
belovdg <belovdg87@yandex.ru>
2020-12-08 22:52:54 +0300

    Initial commit

---
+++ src/Studying/Main.java
@@ -0,0 +1,17 @@
+package Studying;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int i = 4;
+        if (i > 0) {System.out.println(i + " положительное и ");
+        }
+        else {System.out.println(i + " отрицательное и ");
+        }
+
+        if ( i % 2 == 0 ){System.out.println("четное");
+        }
+        else {System.out.println("нечетное");
+        }
+    }
+}
