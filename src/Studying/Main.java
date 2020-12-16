61c72d0baee10b8e25ab716eaf9c4917d668eae9
belovdg <belovdg87@yandex.ru>
2020-12-16 16:08:54 +0300

    Добавлена проверка 0

--- src/Studying/Main.java
+++ src/Studying/Main.java
@@ -3,7 +3,11 @@ package Studying;
 public class Main {
 
     public static void main(String[] args) {
-        int i = 4;
+        int i = 0;
+        if (i == 0) {System.out.println("Значение 0");
+            return;
+        }
+
         if (i > 0) {System.out.println(i + " положительное и ");
         }
         else {System.out.println(i + " отрицательное и ");
